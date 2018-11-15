b38f9a8aa0ea60826f943a11032ce6305b80faf1
Ario Tamaddoni <ariozone6@gmail.com>
2018-11-15 14:44:02 -0800

    A practice link added to navbar.#6

--- src/navbar.js
+++ src/navbar.js
@@ -5,11 +5,13 @@ export default function Navbar() {
     <nav className="navbar-expand-lg navbar-light bg-light mx-auto mb-5 p-3 border-light shadow-lg" id="navbarColor03">
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav">
-          <a className="navbar-brand"><i className="fas fa-user-astronaut px-3 fa-2x"></i>QuizSpace</a>
+          <a className="navbar-brand"><i className="fas fa-user-astronaut px-3 fa-2x"></i>MyQuizSpace</a>
           <li className="nav-item">
             <a className="nav-link text-center" href="#list">My Flashcards</a></li>
           <li className="nav-item ">
-            <a className="nav-link text-center" href="#create">Create a Flashcard</a></li></ul>
+            <a className="nav-link text-center" href="#create">Create a Flashcard</a></li>
+          <li className="nav-item ">
+            <a className="nav-link text-center" href="#create">Practice</a></li></ul>
       </div>
     </nav>
   )
