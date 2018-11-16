e86d296db4030b7bb761bdc62892d6581ad91775
Ario Tamaddoni <ariozone6@gmail.com>
2018-11-15 16:40:39 -0800

    The Practice class component created to renders one card and left and right arrows.#6

--- src/practice.js
+++ src/practice.js
@@ -1,6 +1,27 @@
 import React from 'react'
 export default class Practice extends React.Component {
   render() {
-    return (<h1>Practice</h1>)
+    return (
+      <div className="container p-2">
+        <div className="row d-flex justify-content-center">
+          <span>
+            <i
+              className="fas fa-arrow-left fa-5x mr-3 mt-5"
+              onClick={this.toLeft}></i>
+          </span>
+          <div className="card w-50 text-center shadow-lg border-light">
+            <div className="card-body">
+              <h6 className="card-title mt-3">Question</h6>
+              <p className="card-text">Answer</p>
+            </div>
+          </div>
+          <span>
+            <i
+              className="fas fa-arrow-right fa-5x ml-3 mt-5"
+              onClick={this.toRight}></i>
+          </span>
+        </div>
+      </div>
+    )
   }
 }
