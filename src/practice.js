74974e5e45897d59af837c794dfc2e1a365c5485
Ario Tamaddoni <ariozone6@gmail.com>
2018-11-16 15:40:56 -0800

    The Practice class component enhanced to show the answer only when the 'show answer' is clicked.#6

--- src/practice.js
+++ src/practice.js
@@ -8,7 +8,7 @@ export default class Practice extends React.Component {
     }
     this.toLeft = this.toLeft.bind(this)
     this.toRight = this.toRight.bind(this)
-    this.viewAnswer = this.viewAnswer.bind(this)
+    this.toggleAnswer = this.toggleAnswer.bind(this)
   }
 
   toLeft() {
@@ -19,7 +19,7 @@ export default class Practice extends React.Component {
     else {
       this.setState({ currentCard: currentCard - 1 })
     }
-
+    this.setState({ viewAnswer: false })
   }
   toRight() {
     const { currentCard } = this.state
@@ -29,6 +29,8 @@ export default class Practice extends React.Component {
     else {
       this.setState({ currentCard: currentCard + 1 })
     }
+    this.setState({ viewAnswer: false })
+
   }
 
   toggleAnswer() {
@@ -37,24 +39,24 @@ export default class Practice extends React.Component {
 
   render() {
     const { cards } = this.props
-    const { currentCard } = this.state
+    const { currentCard, viewAnswer } = this.state
     return (
       <div className="container p-2">
         <div className="row d-flex justify-content-center">
           <span>
             <i
-              className="fas fa-arrow-left fa-5x mr-3 mt-5"
+              className="fas fa-arrow-left fa-4x mr-4 mt-4"
               onClick={this.toLeft}></i>
           </span>
           <div className="card w-50 text-center shadow-lg border-light">
-            <div className="card-body">
-              <h6 className="card-title mt-3">{cards[currentCard].question}</h6>
-              <p className="card-text"><i className="fas fa-unlock fa-2x" onClick={this.toggleAnswer}></i>{cards[currentCard].answer}</p>
+            <div className="card-body py-4">
+              <h6 className="card-title">{cards[currentCard].question}</h6>
+              <p className="card-text mt-4">{viewAnswer ? cards[currentCard].answer : <span className="showAnswer" onClick={this.toggleAnswer}>See Answer  <i className="far fa-eye fa-lg" ></i></span>}</p>
             </div>
           </div>
           <span>
             <i
-              className="fas fa-arrow-right fa-5x ml-3 mt-5"
+              className="fas fa-arrow-right fa-4x ml-3 mt-4"
               onClick={this.toRight}></i>
           </span>
         </div>
