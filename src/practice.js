21c18e81a535f0965cc158970b4f10a40214efbc
Ario Tamaddoni <ariozone6@gmail.com>
2018-11-16 18:47:44 -0800

    A bootswatch progress bar added to render method of Practice class component.#7

--- src/practice.js
+++ src/practice.js
@@ -40,6 +40,7 @@ export default class Practice extends React.Component {
   render() {
     const { cards } = this.props
     const { currentCard, viewAnswer } = this.state
+    const progress = 10
     return (
       <div className="container p-2">
         <div className="row d-flex justify-content-center">
@@ -56,10 +57,16 @@ export default class Practice extends React.Component {
           </div>
           <span>
             <i
-              className="fas fa-arrow-right fa-4x ml-3 mt-4"
+              className="fas fa-arrow-right fa-4x ml-4 mt-4"
               onClick={this.toRight}></i>
           </span>
         </div>
+        <div className="progress w-75 mt-5 mx-auto bg-light shadow-lg">
+
+          <div className="progress-bar progress-bar-striped progress-bar-animated bg-gray" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: progress + '%' }}>
+          </div>
+        </div>
+
       </div>
     )
   }
